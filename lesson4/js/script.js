da736df8b6996374bc055562da75cc6bc44d08eb
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-05-15 11:15:40 -0600

    week4 push 7

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -8,3 +8,6 @@ if (document.lastModified) {
     document.getElementById('lastModified').innerHTML =
         new Date(document.lastModified).toLocaleString();
 }
+
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
+document.getElementById('currentdate').textContent = new DataCue().toLocaleString('en-US', options);
