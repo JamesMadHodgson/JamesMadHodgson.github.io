e096d9228e381b0d08c0cd794177964840770812
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-05-15 11:18:05 -0600

    week4 push 8

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -4,10 +4,6 @@ const mainnav = document.querySelector('.navigation')
 hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 
 
-if (document.lastModified) {
-    document.getElementById('lastModified').innerHTML =
-        new Date(document.lastModified).toLocaleString();
-}
 
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
 document.getElementById('currentdate').textContent = new DataCue().toLocaleString('en-US', options);
