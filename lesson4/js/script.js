c0efb00e58927276eabe867ed3bd26faae2af04f
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-05-15 15:53:39 -0600

    push final for date

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,9 +1,9 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('.navigation');
 
 hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 
 
 
-const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Data().toLocaleString('en-US', options);
