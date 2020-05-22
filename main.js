a95abf09278d70db1f39029bc9e48c345bfdb074
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-05-22 12:09:28 -0600

    week 5 push 5

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-if(document.lastModified){
-    document.getElementById('lastModified').innerHTML=
-    new Date(document.lastModified).toLocaleString();
+/* hamburger*/
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+
+
+/* day of the week */
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+/* banner
+if (WeekDay[d.getDay()] == "Sunday") {
+    document.getElementById("banner").style.display = 'block';
+} else {
+    document.getElementById("banner").style.display = 'none';
+} */
+let d = new Date();
+const banner =
+    document.getElementById('banner');
+if (d.getDay() == 5) {
+    document.getElementById('banner').style.display = 'block';
+} else {
+    document.getElementById('banner').style.display = 'none';
 }
