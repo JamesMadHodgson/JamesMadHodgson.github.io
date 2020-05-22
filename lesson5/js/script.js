4a5da61c5c33c5ca65e87abd6c59d32f4899ef81
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-05-22 12:14:07 -0600

    week 5 push 6

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -7,3 +7,18 @@ hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive
 
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+/* banner
+if (WeekDay[d.getDay()] == "Sunday") {
+    document.getElementById("banner").style.display = 'block';
+} else {
+    document.getElementById("banner").style.display = 'none';
+} */
+let d = new Date();
+const banner =
+    document.getElementById('banner');
+if (d.getDay() == 5) {
+    document.getElementById('banner').style.display = 'block';
+} else {
+    document.getElementById('banner').style.display = 'none';
+}
--- main.js
+++ main.js
@@ -8,18 +8,3 @@ hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive
 /* day of the week */
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-
-/* banner
-if (WeekDay[d.getDay()] == "Sunday") {
-    document.getElementById("banner").style.display = 'block';
-} else {
-    document.getElementById("banner").style.display = 'none';
-} */
-let d = new Date();
-const banner =
-    document.getElementById('banner');
-if (d.getDay() == 5) {
-    document.getElementById('banner').style.display = 'block';
-} else {
-    document.getElementById('banner').style.display = 'none';
-}
