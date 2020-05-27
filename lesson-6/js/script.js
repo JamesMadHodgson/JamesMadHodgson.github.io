04d5f7af25c07efa3c8857261dec11bb8a0bae65
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-05-27 11:44:29 -0600

    week6 push 2

--- lesson-6/js/script.js
+++ lesson-6/js/script.js
@@ -1,20 +1,20 @@
-/* hamburger*/
+/*hamburger*/
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation');
 
 hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 
+/*date*/
 
-/* day of the week */
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
-
-/*web font reference */
-WebFont.load({
-    google: {
-        families: [
-            'Acme'
-        ]
-    }
-});
+/* banner */
+let d = new Date();
+const banner =
+    document.getElementById('banner');
+if (d.getDay() == 5) {
+    document.getElementById('banner').style.display = 'block';
+} else {
+    document.getElementById('banner').style.display = 'none';
+}
