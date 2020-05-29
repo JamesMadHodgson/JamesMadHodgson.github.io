c1bd947df0cfb70496dff1662aac0970451f26f4
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-05-28 18:54:36 -0600

    week 6

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -19,5 +19,27 @@ if (d.getDay() == 5) {
     document.getElementById('banner').style.display = 'none';
 }
 
-/* wind chill */
-let w
+
+/*web font reference */
+WebFont.load({
+    google: {
+        families: [
+            'Crimson Text', 'serif', 'Crimson Text', 'serif'
+        ]
+    }
+});
+
+/*windchill*/
+
+let t = document.getElementById('temp').textContent;
+let s = document.getElementById('speed').textContent;
+let x = 'N/A';
+
+if (t < 50 && s > 3) {
+    let y = Math.pow(s, 0.16);
+    let f = 35.74 + 0.6215 * t - 35.75 * y + 0.4275 * t * y;
+    let x = f.toFixed(0);
+    document.getElementById('chill').textContent = x;
+} else {
+    document.getElementById('chill').textContent = 'N/A';
+}
--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-/* hamburger*/
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation');
-
-hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
-
-
 /* day of the week */
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
