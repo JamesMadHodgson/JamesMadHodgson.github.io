ddcbbd194fae91b66ae10cb3765c7062659a62e3
JamesMadHodgson <JamesMadHodgson.github.io/cit261>
2020-06-25 16:20:53 -0600

    week 10 1st push

---
+++ lesson-10/js/preston.js
@@ -0,0 +1,56 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=38246ebcfa743f03eef5e9626ed4d208";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current').textContent = jsObject.weather[0].main;
+        document.getElementById('temp').textContent = jsObject.main.temp;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('speed').textContent = jsObject.wind.speed;
+
+    });
+
+
+/*windchill*/
+/*let t = parseFloat(document.getElementById("temp").textContent);
+let s = parseFloat(document.getElementById("speed").textContent);
+*/
+let t = document.getElementById('temp').textContent;
+let s = document.getElementById('speed').textContent;
+
+let x = 'N/A';
+
+if (t <= 50 && s >= 3) {
+    let y = Math.pow(s, 0.16);
+    let f = 35.74 + 0.6215 * t - 35.75 * y + 0.4275 * t * y;
+    let x = f.toFixed(0);
+    document.getElementById('chill').textContent = x;
+} else {
+    document.getElementById('chill').textContent = 'N/A';
+}
+
+const forecastApiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=38246ebcfa743f03eef5e9626ed4d208";
+fetch(forecastApiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const fivedayforecast = jsObject.list.filter(f => f.dt_txt.includes('18:00:00'));
+        console.log(fivedayforecast);
+
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thru', 'Fri', 'Sat'];
+
+
+
+        let day = 0;
+        fivedayforecast.forEach(forecast => {
+            let d = new Date(forecast.dt_txt);
+            document.getElementById(`data${day+1}`).textContent = forecast.main.temp;
+            document.getElementById(`label${day+1}`).textContent = weekdays[d.getDay()];
+            day++;
+
+
+        });
+
+    });
---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=38246ebcfa743f03eef5e9626ed4d208";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.weather[0].description; // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+    });
